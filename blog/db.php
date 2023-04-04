ca7a5472c5036ec9ad08a0d1ff9ee612f3a96277
abakar-adam-abdallah <114398897+abakar-adam-abdallah@users.noreply.github.com>
2023-04-04 18:00:13 +0200

    Delete blog directory

--- blog/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host    = 'localhost';
-$user    = 'root';
-$pass    = 'root';
-$db_name = 'blogs';
-
-$conn = new PDO('mysql:host='.$host.';dbname='.$db_name.';charset=utf8', $user,$pass,[
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
